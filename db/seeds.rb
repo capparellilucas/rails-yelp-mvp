116573cb940c8d6c05334132a177eebb94c8f5ac
lucas capparelli <capparelli.lucas@gmail.com>
2022-08-17 14:05:35 -0300

    2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.create(name: "Star Wars1", address: "Rivadavia1", phone_number: "1568713511", category: "chinese")
+Restaurant.create(name: "Star Wars2", address: "Rivadavia2", phone_number: "1568713522", category: "chinese")
+Restaurant.create(name: "Star Wars3", address: "Rivadavia3", phone_number: "1568713533", category: "italian")
+Restaurant.create(name: "Star Wars4", address: "Rivadavia4", phone_number: "1568713544", category: "french")
+Restaurant.create(name: "Star Wars5", address: "Rivadavia"5, phone_number: "1568713555", category: "french")
